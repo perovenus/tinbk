f34aa4736cf7b386a30b2dc62e4ef69ba0a8bc5a
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2022-04-25 21:49:22 +0700

    up Item.js

--- app/screen/Item.js
+++ app/screen/Item.js
@@ -1,20 +1,37 @@
+import { faAlignJustify } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
 import {
   View,
-  StyleSheet
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Text
 } from 'react-native'
+
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
 const Item = ({item}) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.imagecontainer}>
-
-      </View>
-      <View styles={styles.details}>
-
-      </View>
-    </View> 
+    <View>
+      <TouchableOpacity>
+        <View style={styles.container}>
+          <Image
+            resizeMode='contain'
+            source={require('../assets/hakyu.jpg')} 
+            style={styles.image}
+          />
+          <View style={styles.details}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.price}>{item.price}</Text>
+            <Text style={styles.location}>{item.location}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.buybutton}>
+        <FontAwesome5 name='shopping-cart' size={24} color='#2f80ed'/>
+      </TouchableOpacity>
+    </View>
   )
 }
 
@@ -23,14 +40,42 @@ export default Item;
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    height: 170,
-    backgroundColor: 'red',
-    marginBottom: 10
+    height: 140,
+    marginHorizontal: 5,
+    marginTop: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#F0F0F0',
+    flexDirection: 'row',
+    alignItems: 'center'
   },
-  imagecontainer: {
-
+  image: {
+    width: '35%',
+    height: '90%',
   },
   details: {
+    width: '65%',
+    height: '90%',
+  },
+  name: {
+    fontSize: 17,
+    fontWeight: '400',
+    color: 'rgb(36, 36, 36)',
+  },
+  price: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#303030',
+    marginTop: 5,
+  },
+  location: {
+    fontSize: 15,
+    marginTop: 40,
+    color: '#5A5A5A'
 
+  },
+  buybutton: {
+    position: 'absolute',
+    top: '75%',
+    left: '86%',
   }
 })
