70fe97e29efd9891d2995e304e1283ee1a8880d9
Hai Nguyen Dang <zelvor1307@gmail.com>
2022-04-25 21:32:48 +0700

    upload

--- App.js
+++ App.js
@@ -5,7 +5,11 @@
  import ConfirmOTPScreen from './app/screen/ConfirmOTPScreen';
  import Notification from './app/screen/Notification'
  import Tabs from './app/screen/Tabs'
+<<<<<<< Updated upstream
  import UploadProduct from './app/screen/UploadProduct';
+=======
+ import UploadProduct from './app/screen/UploadProduct'
+>>>>>>> Stashed changes
  import { Router, Scene } from 'react-native-router-flux'
  
  export default function App(props) {
@@ -17,7 +21,11 @@
         <Scene key = "getOTPScreen" component = {GetOTPScreen} title = "GetOTP" hideNavBar={true}/>
         <Scene key = "confirmOTPScreen" component = {ConfirmOTPScreen} title = "ConfirmOTP" hideNavBar={true}/>
         <Scene key = "Tabs" component = {Tabs} title = "Tabs" hideNavBar={true}/>
+<<<<<<< Updated upstream
         <Scene key = "uploadProduct" component = {UploadProduct} title = "UploadProduct" initial = {true} hideNavBar={true} />
+=======
+        <Scene key = "UploadProduct" component = {UploadProduct} initial = {true} title = "UploadProduct" hideNavBar={true}/>
+>>>>>>> Stashed changes
       </Scene>
     </Router>
    );
