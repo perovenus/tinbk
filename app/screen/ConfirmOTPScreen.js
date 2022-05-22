ff4b61c6e4deb0f59180c0e2eba548a047501296
Hai Nguyen Dang <zelvor1307@gmail.com>
2022-05-22 17:36:24 +0700

    linh tinh

--- app/screen/ConfirmOTPScreen.js
+++ app/screen/ConfirmOTPScreen.js
@@ -1,84 +1,86 @@
-import React from 'react'
-import { 
+import React from 'react';
+import {
   StyleSheet,
-  Text, 
-  View, 
-  TextInput, 
-  TouchableOpacity, 
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
   ImageBackground,
   Dimensions,
-  Pressable
-  } from 'react-native'
-import { Actions } from 'react-native-router-flux'
+  Pressable,
+} from 'react-native';
+import {Actions} from 'react-native-router-flux';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default function ConfirmOTPScreen() {
-    const goToGetOTPScreen = () => {
-        Actions.getOTPScreen()
-    }
+  const goToGetOTPScreen = () => {
+    Actions.getOTPScreen();
+  };
 
   return (
     <ImageBackground
       style={styles.background}
-      source={require('../assets/getotp-background.jpg')}
-    >
+      source={require('../assets/getotp-background.jpg')}>
       <TouchableOpacity onPress={goToGetOTPScreen} style={styles.gobackButton}>
         <View>
-          <FontAwesome5
-            name='angle-left'
-            color='white'
-            size={28} 
-          />
-        </View>
-      </TouchableOpacity>  
-      <Text 
-        style={{
-          fontSize: 36,
-          color: '#2F80ED',
-          marginLeft: 30,
-        }}>
-        Nhập mã OTP
-      </Text>
-      <View style={styles.accountInfo}>
-        <Text style={{
-          fontSize: 20,
-          color: '#2F80ED'
-        }}>
-          Chúng tôi đã gửi mã OTP, vui lòng đợi và kiểm tra mail của bạn
-        </Text>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <TextInput style={styles.input} />
-            <TextInput style={styles.input} />
-            <TextInput style={styles.input} />
-            <TextInput style={styles.input} />
-            <TextInput style={styles.input} />
-            <TextInput style={styles.input} />
-        </View>
-      
-      </View>
-      <TouchableOpacity>
-        <View style={[styles.loginButton, styles.elevation]}>
-          <Text style={{fontSize:24, color: 'white'}}>Xác nhận</Text>
+          <FontAwesome5 name="angle-left" color="white" size={28} />
         </View>
       </TouchableOpacity>
-      <Pressable>
-        {({pressed}) => (
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 36,
+            color: '#2F80ED',
+            marginLeft: 30,
+            fontWeight: 'bold',
+          }}>
+          Nhập mã OTP
+        </Text>
+        <View style={styles.accountInfo}>
           <Text
-            style={[
-              {textDecorationLine: pressed ? 'underline' : 'none'},
-              {color: pressed ? '#6c5ce7' : '#7f8c8d'},
-              styles.resendotp
-            ]}>
-            Gửi lại mã
+            style={{
+              fontSize: 24,
+              color: '#2F80ED',
+            }}>
+            Chúng tôi đã gửi mã OTP, vui lòng đợi và kiểm tra mail của bạn
           </Text>
-        )}
-      </Pressable>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <TextInput style={styles.input} />
+            <TextInput style={styles.input} />
+            <TextInput style={styles.input} />
+            <TextInput style={styles.input} />
+            <TextInput style={styles.input} />
+            <TextInput style={styles.input} />
+          </View>
+        </View>
+        <TouchableOpacity>
+          <View style={[styles.loginButton, styles.elevation]}>
+            <Text style={{fontSize: 24, color: 'white'}}>Xác nhận</Text>
+          </View>
+        </TouchableOpacity>
+        <Pressable>
+          {({pressed}) => (
+            <Text
+              style={[
+                {textDecorationLine: pressed ? 'underline' : 'none'},
+                {color: pressed ? '#6c5ce7' : '#7f8c8d'},
+                styles.resendotp,
+              ]}>
+              Gửi lại mã
+            </Text>
+          )}
+        </Pressable>
+      </View>
     </ImageBackground>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  background: { 
+  container: {
+    marginTop: 80,
+    marginHorizontal: 10,
+  },
+  background: {
     paddingTop: 150,
     position: 'absolute',
     left: 0,
@@ -86,7 +88,7 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
-  accountInfo:{
+  accountInfo: {
     marginHorizontal: 30,
     marginTop: 35,
     height: 125,
@@ -98,10 +100,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 12,
     borderColor: '#2F80ED',
-    fontSize: 15
+    fontSize: 25,
+    textAlign: 'center',
   },
   loginButton: {
-    marginTop: 35,
+    marginTop: 60,
     width: 145,
     height: 60,
     borderWidth: 1,
@@ -119,12 +122,12 @@ const styles = StyleSheet.create({
   gobackButton: {
     position: 'absolute',
     marginLeft: 15,
-    marginTop: 21
+    marginTop: 21,
   },
   resendotp: {
     alignSelf: 'center',
-    fontSize: 16,
+    fontSize: 20,
     marginTop: 15,
-    fontWeight: 'bold'
-  }
-})
+    fontWeight: 'bold',
+  },
+});
