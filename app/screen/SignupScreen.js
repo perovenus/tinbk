32c7778e2fd26b093813b83a78063141590aa6b5
Hai Nguyen Dang <zelvor1307@gmail.com>
2022-06-02 16:23:52 +0700

    Update SignupScreen.js

--- app/screen/SignupScreen.js
+++ app/screen/SignupScreen.js
@@ -364,7 +364,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 12,
     borderColor: '#2F80ED',
-    fontSize: 18,
+    fontSize: 14,
     paddingHorizontal: 15,
     color: '#000',
   },
