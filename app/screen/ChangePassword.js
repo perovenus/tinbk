eae16079f53de68c4a1b5e34715f2cb5e3447ebc
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2022-06-02 10:46:56 +0700

    update

--- app/screen/ChangePassword.js
+++ app/screen/ChangePassword.js
@@ -118,7 +118,7 @@ const ChangePassword = () => {
               onChangeText={text => {
                 setOld_pass(text);
               }}
-              secureTextEntry={false}
+              secureTextEntry={true}
             />
           </View>
           <View style={styles.accountInfo}>
--- app/screen/UploadProduct.js
+++ app/screen/UploadProduct.js
@@ -265,7 +265,7 @@ const UploadProduct = ({item}) => {
                 }}>
                 <View style={styles.centeredView}>
                   <View style={styles.modalView}>
-                    <Text style={styles.modalText}>Đăng ký bán thành công</Text>
+                    <Text style={styles.modalText}>Đăng bán thành công</Text>
                     <Pressable
                       style={[styles.buttonmodal, styles.buttonClose]}
                       onPress={() => setModalVisible(!modalVisible)}>
